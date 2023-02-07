cd5fb9011d83f781ad5c8ee264d3fcc50874f390
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-02-07 07:48:03 -0600

    creando clase SayHello

---
+++ src/SayHello.java
@@ -0,0 +1,6 @@
+public class SayHello {
+    public static void main(String[] args) {
+        String name="Maria Guadalupe Villasana Salgado";
+        System.out.println("Hola mundo " + name);
+    }
+}
