8016d2cb58f9bde7eacafcaabc70759eabb4604a
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-02-14 23:03:02 -0600

    se crea clase PediatricOficce

---
+++ src/PediatricOficce.java
@@ -0,0 +1,8 @@
+public class PediatricOficce {
+    private String name;  //Maria,Ian,Guadalupe,Jesus
+    private double wight;  //2.5kg,3kg,6.5kg,
+    private byte age;   //1,3,5,7,9
+    private String gender;    //Niño,Niña
+    private Boolean disability;    //Verdadero,Falso
+}
+
